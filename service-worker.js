5fb5403ccd7166159614c0434e8a38024b30f659
Nutellandia <164498241+Nutellandia@users.noreply.github.com>
2025-12-02 09:52:32 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const CACHE_NAME='neuro-genesis-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])))})
